3912d95fb819d8f9770ad6b69158cad185288689
bug <bug@dlestra.ws>
2015-10-02 22:00:31 -0600

    convert hw3 into an App, with text inputs

--- Flocking.elm
+++ Flocking.elm
@@ -10,45 +10,80 @@ import Color exposing (grey)
 
 --- Structures ---
 
+type alias Parameters = {
+  maxA : Float,
+  maxV : Float,
+  separation : Float,
+  alignment : Float,
+  coherence : Float,
+  neighborhoodRadius : Float
+}
+
 type alias Simulation = {
+  params : Parameters,
   boids : List Actor
 }
 
-type alias FlockBehavior = Actor -> List Actor -> Vec2
+type alias FlockBehavior = Float -> Actor -> List Actor -> Vec2
+
+
+--- Constants ---
+
+defaults : Parameters
+defaults = {
+  maxA = 70,
+  maxV = 120,
+  separation = 15,
+  alignment = 15,
+  coherence = 0.2,
+  neighborhoodRadius = 90
+ }
 
 
 --- Behavior ---
 
 -- gets all boids in flock within given radius
-neighborhood : List Actor -> Float -> Actor -> List Actor
-neighborhood flock radius boid =
-  List.filter (\a -> dist a.pos boid.pos <= radius) flock
+neighborhood : Float -> Actor -> List Actor -> List Actor
+neighborhood r boid = List.filter
+  (\a -> a.pos /= boid.pos && dist a.pos boid.pos <= r)
 
 -- avoid a neighbor more the closer it is
-avoision : Actor -> Actor -> Vec2
-avoision boid neighbor = let
-  s = neighbor.pos .-. boid.pos
+avoision : Float -> Actor -> Actor -> Vec2
+avoision r boid neighbor = let
+  s = boid.pos .-. neighbor.pos
  in
-  s ./ sqnorm s
+  r *. s ./ sqnorm s
+
+fixNaN : Float -> Float
+fixNaN a = if isNaN a then 0 else a
+
+fixNaN2 : Vec2 -> Vec2
+fixNaN2 (x, y) = (fixNaN x, fixNaN y)
 
 -- steering away from neighbors, inversely related to distance
 separation : FlockBehavior
-separation boid = List.map (avoision boid) >> List.foldl (.+.) (0,0)
+separation r boid = neighborhood r boid
+  >> List.map (avoision r boid)
+  >> List.foldl (.-.) (0, 0)
 
 -- steering toward the average of neighbors' directions
 alignment : FlockBehavior
-alignment boid = List.foldl (\a (dirs, n) -> (dirs .+. normalize a.v, n+1))
-  ((0,0),0) >> uncurry (./)
+alignment r boid = neighborhood r boid
+  >> List.foldl (\a dirs -> dirs .+. a.v) (0, 0)
+  >> clamp2 0 1
 
 -- steering toward the average of neighbors' positions
 cohesion : FlockBehavior
-cohesion boid = List.foldl (\a (poss, n) -> (poss .+. a.pos, n+1))
-  ((0,0),0) >> uncurry (./) >> (flip (.-.)) boid.pos
+cohesion r boid = neighborhood r boid
+  >> List.foldl (\a poss -> poss .+. (a.pos .-. boid.pos)) (0, 0)
+  >> clamp2 0 1
 
 -- combine the above, weighted
 flocking : Float -> Float -> Float -> FlockBehavior
-flocking a b c boid boids = [(a, separation), (b, alignment), (c, cohesion)]
-  |> List.foldl (\(coef, beh) sum -> sum .+. coef *. beh boid boids) (0,0)
+flocking a b c r boid flock =
+  a *. separation (r / 3) boid flock .+.
+  b *. alignment r boid flock .+.
+  c *. cohesion (r / 2) boid flock
 
 
 --- Simulation ---
@@ -56,21 +91,26 @@ flocking a b c boid boids = [(a, separation), (b, alignment), (c, cohesion)]
 initSim : Seed -> Simulation
 initSim seed0 = let
   rand1 = Random.pair (Random.float -200 200) (Random.float -200 200)
-  (poss, seed1) = generate (Random.list 50 rand1) seed0
+  (poss, seed1) = generate (Random.list 60 rand1) seed0
   rand2 = Random.pair (Random.float -30 30) (Random.float -30 30)
-  (vs, seed2) = generate (Random.list 50 rand2) seed1
- in
-  { boids = List.map2 (\pos v -> { pos = pos, v = v, a = (0,0) }) poss vs }
+  (vs, seed2) = generate (Random.list 60 rand2) seed1
+ in {
+   params = defaults,
+   boids = List.map2 (\pos v -> { pos = pos, v = v, a = (0, 0) }) poss vs
+ }
 
 simulate : Time -> Simulation -> Simulation
 simulate t sim = let
   dt = (inSeconds t)
- in { sim |
-  boids <- List.map (\boid ->
-      { boid | a <- flocking 1 2 3 boid sim.boids } -- |> stepActor 50 dt
-    ) sim.boids
- }
+  {separation, alignment, coherence, neighborhoodRadius} = sim.params
+  neighbors = sim.boids
+ in { sim | boids <- List.map (\boid -> { boid | a <-
+        flocking separation alignment coherence neighborhoodRadius
+          boid sim.boids |> clamp2 0 80
+      } |> stepActor 120 dt |> worldWrap ) sim.boids }
 
+worldWrap : Actor -> Actor
+worldWrap boid = { boid | pos <- wrap2 (-200, -200) (200, 200) boid.pos }
 
 --- Drawing ---
 drawSim : Simulation -> Element
--- hw3.elm
+++ hw3.elm
@@ -1,6 +1,59 @@
-import Time exposing (fps)
-import Signal exposing (foldp, map, (<~))
+import Time exposing (Time, fps)
+import Signal exposing (message, (<~))
 import Random exposing (initialSeed)
-import Flocking exposing (Simulation, simulate, initSim, drawSim)
+import Flocking exposing (Simulation, simulate, initSim, drawSim, defaults)
+import Html exposing (..)
+import Html.Attributes exposing (value)
+import Html.Events exposing (on, targetValue)
+import Effects exposing (Effects)
+import StartApp exposing (..)
+import String
 
-main = drawSim <~ foldp simulate (initSim (initialSeed 1337)) (fps 60)
+type Action =
+  Tick Time
+  | Separate Float
+  | Align Float
+  | Cohere Float
+  | Nop
+
+flockInput address default actionType = input [
+  on "input" targetValue
+    (\a -> Signal.message address (case (String.toFloat a) of
+      Ok c -> actionType c
+      Err s -> Nop)),
+  value (toString default) ] []
+
+view address sim = div [] [
+  drawSim sim |> fromElement,
+  hr [] [],
+  dl [] [
+    dt [] [text "separation"],
+     dd [] [flockInput address defaults.separation Separate],
+    dt [] [text "alignment"],
+     dd [] [flockInput address defaults.alignment Align],
+    dt [] [text "coherence"],
+     dd [] [flockInput address defaults.coherence Cohere]
+   ]
+ ]
+
+update action sim = let params = sim.params in
+ (flip (,)) Effects.none <| case action of
+  Tick dt -> simulate dt sim
+  Separate c -> { sim | params <- { params | separation <- c } }
+  Align c -> { sim | params <- { params | alignment <- c } }
+  Cohere c -> { sim | params <- { params | coherence <- c } }
+
+sim : Simulation
+sim = initSim (initialSeed 1337)
+
+none : Effects Action
+none = Effects.none
+
+app = start {
+  init = ( sim, none ),
+  update = update,
+  view = view,
+  inputs = [ Tick <~ (fps 60) ]
+ }
+
+main = app.html
