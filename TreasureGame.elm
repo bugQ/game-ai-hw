c365f00600b054149a121d32fc4866cbace4c593
bug <bug@dlestra.ws>
2015-10-31 08:52:18 -0600

    start TreasureGame

--- TreasureGame.elm
+++ TreasureGame.elm
@@ -2,3 +2,19 @@ module TreasureGame where
 
 import Grid
 
+gridW = 20
+gridH = 20
+spacing = 30
+
+
+
+initSim seed0 = let
+  (startingGrid, seed1) = Grid.randGrid gridW gridH spacing seed0
+  indices = Array.initialize (Array.length grid.array) identity
+  (randIndices, seed2) = shuffle seed1 indices
+  openIndices = Array.filter
+    (\i -> Array.get i grid.array /= Just Obstacle) randIndices
+  key1 :: key2 :: key3 :: door1 :: door2 :: door3 :: treasure :: [] =
+    Array.toList openIndices |> List.take 7 |> List.map ((flip Grid.deindex) grid)
+in
+  {}
