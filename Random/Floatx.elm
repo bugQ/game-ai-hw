828efefbf498a4bfe51d57bbba0850164c6904dc
bug Q dlestra <bug@dlestra.ws>
2016-08-09 14:30:46 -0600

    temporary change to standardNormal because Random.float is still broken

--- Random/Floatx.elm
+++ Random/Floatx.elm
@@ -4,4 +4,4 @@ import Random exposing (Generator)
 
 standardNormal : Generator Float
 standardNormal = Random.map2 (\u theta -> sqrt (-2 * logBase e u) * cos theta)
-  (Random.float 0 1) (Random.float 0 (pi+pi))
+  (Random.float (2 ^ -33) 1) (Random.float 0 (pi+pi))
